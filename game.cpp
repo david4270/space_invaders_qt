1cc17722124d3efd2d0ff617369febb880b85fe6
David Song <david990724@gmail.com>
2023-08-23 22:28:46 -0400

    minor edit

--- game.cpp
+++ game.cpp
@@ -55,9 +55,9 @@ void Game::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Left){
         //qDebug() << "Left" << 800 *0/numPlayers+5;;
         if(players[0]){
-            if(players[0]->pos().x() > 800 *0/numPlayers+5){
+            if(players[0]->x() > 800 *0/numPlayers+5){
                 //qDebug() << "Moving left";
-                players[0]->setPos(players[0]->pos().x()-5 ,players[0]->pos().y());
+                players[0]->setPos(players[0]->x()-5 ,players[0]->y());
             }
         }
 
@@ -65,9 +65,9 @@ void Game::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Right){
         //qDebug() << "Right" << 800 *1/numPlayers-5;
         if(players[0]){
-            if(players[0]->pos().x() < 800 * 1/numPlayers-5){
+            if(players[0]->x() < 800 * 1/numPlayers-5){
                 //qDebug() << "Moving right";
-                players[0]->setPos(players[0]->pos().x()+5 ,players[0]->pos().y());
+                players[0]->setPos(players[0]->x()+5 ,players[0]->y());
             }
         }
 
@@ -75,22 +75,22 @@ void Game::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Up){
         if(players[0]){
             Bullet * bullet = new Bullet();
-            bullet->setPos(players[0]->pos().x(),players[0]->pos().y());
+            bullet->setPos(players[0]->x(),players[0]->y());
             scene->addItem(bullet);
         }
 
     }
     if(event->key() == Qt::Key_A && numPlayers > 1){
         if(players[1]){
-            if(players[1]->pos().x() > 800 * 1/numPlayers + 5){
-                players[1]->setPos(players[1]->pos().x()-5 ,players[1]->pos().y());
+            if(players[1]->x() > 800 * 1/numPlayers + 5){
+                players[1]->setPos(players[1]->x()-5 ,players[1]->y());
             }
         }
     }
     if(event->key() == Qt::Key_D && numPlayers > 1){
         if(players[1]){
-            if(players[1]->pos().x() < 800 * 2/numPlayers - 5){
-                players[1]->setPos(players[1]->pos().x()+5 ,players[1]->pos().y());
+            if(players[1]->x() < 800 * 2/numPlayers - 5){
+                players[1]->setPos(players[1]->x()+5 ,players[1]->y());
             }
         }
     }
@@ -103,43 +103,43 @@ void Game::keyPressEvent(QKeyEvent *event){
     }
     if(event->key() == Qt::Key_J && numPlayers > 2){
         if(players[2]){
-            if(players[2]->pos().x() > 800 * 2/numPlayers + 5){
-                players[2]->setPos(players[2]->pos().x()-5 ,players[2]->pos().y());
+            if(players[2]->x() > 800 * 2/numPlayers + 5){
+                players[2]->setPos(players[2]->x()-5 ,players[2]->y());
             }
         }
     }
-    else if(event->key() == Qt::Key_L && numPlayers > 2){
+    if(event->key() == Qt::Key_L && numPlayers > 2){
         if(players[2]){
-            if(players[2]->pos().x() < 800 * 3/numPlayers -5){
-                players[2]->setPos(players[2]->pos().x()+5 ,players[2]->pos().y());
+            if(players[2]->x() < 800 * 3/numPlayers -5){
+                players[2]->setPos(players[2]->x()+5 ,players[2]->y());
             }
         }
     }
     if(event->key() == Qt::Key_I && numPlayers > 2){
         if(players[2]){
             Bullet * bullet = new Bullet();
-            bullet->setPos(players[2]->pos().x(),players[2]->pos().y());
+            bullet->setPos(players[2]->x(),players[2]->y());
             scene->addItem(bullet);
         }
     }
     if(event->key() == Qt::Key_Delete && numPlayers > 3){
         if(players[3]){
-            if(players[3]->pos().x() > 800 * 3/numPlayers + 5){
-                players[3]->setPos(players[3]->pos().x()-5 ,players[3]->pos().y());
+            if(players[3]->x() > 800 * 3/numPlayers + 5){
+                players[3]->setPos(players[3]->x()-5 ,players[3]->y());
             }
         }
     }
     if(event->key() == Qt::Key_PageDown && numPlayers > 3){
         if(players[3]){
-            if(players[3]->pos().x() < 800 * 4/numPlayers -5){
-                players[3]->setPos(players[3]->pos().x()+5 ,players[3]->pos().y());
+            if(players[3]->x() < 800 * 4/numPlayers -5){
+                players[3]->setPos(players[3]->x()+5 ,players[3]->y());
             }
         }
     }
     if(event->key() == Qt::Key_Home && numPlayers > 3){
         if(players[3]){
             Bullet * bullet = new Bullet();
-            bullet->setPos(players[3]->pos().x(),players[3]->pos().y());
+            bullet->setPos(players[3]->x(),players[3]->y());
             scene->addItem(bullet);
         }
     }
